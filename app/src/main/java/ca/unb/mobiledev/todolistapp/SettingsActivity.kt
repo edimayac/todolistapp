0a36c4cc1221d174980dcabd3ff12b2c43711b74
thlgrc <epadilla@unb.ca>
2022-12-03 14:49:29 -0400

    BottomNavigationBar

--- app/src/main/java/ca/unb/mobiledev/todolistapp/SettingsActivity.kt
+++ app/src/main/java/ca/unb/mobiledev/todolistapp/SettingsActivity.kt
@@ -1,25 +1,14 @@
 package ca.unb.mobiledev.todolistapp
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.text.Layout
 import android.view.MenuItem
-import android.view.View
-import android.widget.SimpleAdapter.ViewBinder
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
-import ca.unb.mobiledev.todolistapp.databinding.ActivityMainBinding
 import ca.unb.mobiledev.todolistapp.databinding.ActivitySettingsBinding
-import ca.unb.mobiledev.todolistapp.databinding.ActivitySummaryBinding
-import com.google.android.material.navigation.NavigationView
-import com.google.android.material.navigation.NavigationView.OnNavigationItemSelectedListener
 
 class SettingsActivity : AppCompatActivity () {
     private lateinit var binding: ActivitySettingsBinding
-    private lateinit var itemClicked: MenuItem
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,26 +17,40 @@ class SettingsActivity : AppCompatActivity () {
         // Bottom Navigation Bar
         binding = ActivitySettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
-//            setContentView(this@SettingsActivity,R.layout.settings,binding)
+        binding.bottomNavigationView.menu.getItem(2).setChecked(true)
+        bottomNavigationBar(this@SettingsActivity)
+
+
+
+
+    }
+
+
+    private fun bottomNavigationBar(context: Context){
+
         binding.bottomNavigationView.setOnItemSelectedListener{item ->(
+
                 when (item.itemId){
 
                     R.id.summary -> {
-                        val intent = Intent(this,SummaryActivity::class.java)
-                        itemClicked = item
+                        val intent = Intent(context,SummaryActivity::class.java)
+                        intent.putExtra("item",item.itemId)
                         startActivity(intent)
+                        finish()
                     }
 
                     R.id.settings -> {
-                        val intent = Intent(this,SettingsActivity::class.java)
-                        itemClicked = item
+                        val intent = Intent(context,SettingsActivity::class.java)
+                        intent.putExtra("item",item.itemId)
                         startActivity(intent)
+                        finish()
                     }
 
                     R.id.list -> {
-                        val intent = Intent(this,MainActivity::class.java)
-                        itemClicked = item
+                        val intent = Intent(context,MainActivity::class.java)
+                        intent.putExtra("item",item.itemId)
                         startActivity(intent)
+                        finish()
                     }
 
                 }
@@ -56,12 +59,6 @@ class SettingsActivity : AppCompatActivity () {
         }
 
 
-
     }
-
-    fun getItemClicked(): MenuItem {
-        return itemClicked
-    }
-
 
 }
